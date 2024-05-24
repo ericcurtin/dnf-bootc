f7bbcd56a6acc28ee37e1070b746224c88ee2fe2
Eric Curtin <ecurtin@redhat.com>
2024-05-24 20:42:42 +0100

    Support remove and install
    Signed-off-by: Eric Curtin <ecurtin@redhat.com>

--- bootc.py
+++ bootc.py
@@ -11,27 +11,32 @@ class BootcPlugin(Plugin):
         super(BootcPlugin, self).__init__(base, cli)
         self.base = base
         self.cli = cli
-        self.pkgs = []
+        self.pkgs_install = []
+        self.pkgs_remove = []
 
     def resolved(self):
-        for pkg in self.base.transaction.install_set:
-            self.pkgs.append(pkg.name)
+        self.pkgs_install = [pkg.name for pkg in self.base.transaction.install_set]
+        self.pkgs_remove = [pkg.name for pkg in self.base.transaction.remove_set]
 
     def transaction(self):
-        if self.pkgs:
-            # This method is called after the transaction is executed
+        actions = []
+        if self.pkgs_install:
+            actions.append(f"RUN dnf install -y {' '.join(self.pkgs_install)}")
+        if self.pkgs_remove:
+            actions.append(f"RUN dnf remove -y {' '.join(self.pkgs_remove)}")
+
+        if actions:
             containerfile_dir = '/var/Containerfile'
             containerfile_contents = ''
             if not exists(containerfile_dir):
                 data = subprocess.run(["bootc", "status"], capture_output=True, text=True, check=True)
-                data = data.stdout
-                data = yaml.safe_load(data)
+                data = yaml.safe_load(data.stdout)
 
                 # Extract the desired image value
                 image = data['spec']['image']['image']
                 containerfile_contents = f"FROM {image}\n"
 
-            containerfile_contents = f"{containerfile_contents}RUN dnf install -y {' '.join(self.pkgs)}\n"
+            containerfile_contents = f"{containerfile_contents}\n".join(actions) + '\n'
             with open(containerfile_dir, 'a') as f:
                 f.write(containerfile_contents)
 
