d5adb32b184b28d3c19ae1030f93e976a84ac9fe
Eric Curtin <ecurtin@redhat.com>
2024-05-25 10:53:24 +0100

    Complete! root user checks
    Signed-off-by: Eric Curtin <ecurtin@redhat.com>

--- install.sh
+++ install.sh
@@ -3,12 +3,18 @@
 main() {
   set -eu -o pipefail
 
+  if [ "$EUID" -ne 0 ]; then
+    echo "Error: This command has to be run with superuser privileges (under the root user on most systems)." >&2
+    exit 1
+  fi
+
   local tmp="$(mktemp -d)"
   local url="raw.githubusercontent.com/ericcurtin/dnf-bootc/main"
   curl -fsSL -o "$tmp/dnf-bootc" "https://$url/dnf-bootc"
   curl -fsSL -o "$tmp/bootc.py" "https://$url/bootc.py"
   install -D -m755 "$tmp/dnf-bootc" "/var/dnf-bootc"
   install -D -m755 "$tmp/bootc.py" "/var/dnf-plugins/bootc.py"
+  echo "Complete!"
   rm -rf $tmp &
 }
 
