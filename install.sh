075b2c8c36aa42b667164cb2392a5d84fb7b533b
Eric Curtin <ecurtin@redhat.com>
2024-05-25 10:46:03 +0100

    URL to url
    Signed-off-by: Eric Curtin <ecurtin@redhat.com>

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 main() {
-  set -eux
+  set -eu -o pipefail
 
   local tmp="$(mktemp -d)"
-  URL="raw.githubusercontent.com/ericcurtin/dnf-bootc/main"
-  curl -fsSL -o "$tmp/dnf-bootc" "https://$URL/dnf-bootc"
-  curl -fsSL -o "$tmp/bootc.py" "https://$URL/bootc.py"
+  local url="raw.githubusercontent.com/ericcurtin/dnf-bootc/main"
+  curl -fsSL -o "$tmp/dnf-bootc" "https://$url/dnf-bootc"
+  curl -fsSL -o "$tmp/bootc.py" "https://$url/bootc.py"
   install -D -m755 "$tmp/dnf-bootc" "/var/dnf-bootc"
   install -D -m755 "$tmp/bootc.py" "/var/dnf-plugins/bootc.py"
   rm -rf $tmp &
