1bc72304843acf9fcfd8ec8e17e03ccd61ab6637
Eric Curtin <ecurtin@redhat.com>
2024-05-25 09:41:28 +0100

    Refactoring
    Script transaction funcion got a bit long
    
    Signed-off-by: Eric Curtin <ecurtin@redhat.com>

--- bootc.py
+++ bootc.py
@@ -19,54 +19,73 @@ class BootcPlugin(Plugin):
         self.pkgs_install = [pkg.name for pkg in self.base.transaction.install_set]
         self.pkgs_remove = [pkg.name for pkg in self.base.transaction.remove_set]
 
+
     def transaction(self):
+        actions = self.generate_actions()
+        if actions:
+            from_line = self.get_os_from_bootc_status()
+            new_containerfile_contents = self.update_containerfile(from_line, actions)
+            self.write_containerfile(new_containerfile_contents)
+            self.build_and_switch_container()
+
+    def generate_actions(self):
         actions = []
         if self.pkgs_install:
             actions.append(f"RUN dnf install -y {' '.join(self.pkgs_install)}")
+
         if self.pkgs_remove:
             actions.append(f"RUN dnf remove -y {' '.join(self.pkgs_remove)}")
 
-        if actions:
-            containerfile = '/var/Containerfile'
-            new_containerfile_contents = ''
-            from_line = ''
+        return actions
 
-            data = subprocess.run(["bootc", "status"], capture_output=True, text=True, check=True)
-            data = yaml.safe_load(data.stdout)
+    def get_os_from_bootc_status(self):
+        data = subprocess.run(["bootc", "status"], capture_output=True, text=True, check=True)
+        data = yaml.safe_load(data.stdout)
+        image = data['spec']['image']['image']
+        return f"FROM {image}\n"
 
-            # Extract the desired image value
-            image = data['spec']['image']['image']
-            from_line = f"FROM {image}\n"
+    def update_containerfile(self, from_line, actions):
+        containerfile = '/var/Containerfile'
+        new_containerfile_contents = ''
+        if exists(containerfile):
+            with open(containerfile, 'r') as f:
+                lines = f.readlines()
 
-            if exists(containerfile):
-                with open(containerfile, 'r') as f:
-                    lines = f.readlines()
+            replaced = False
+            for line in lines:
+                if line.startswith("FROM "):
+                    new_containerfile_contents += from_line
+                    replaced = True
+                else:
+                    new_containerfile_contents += line
 
-                # Replace the FROM line if it exists
-                replaced = False
-                for line in lines:
-                    if line.startswith("FROM "):
-                        new_containerfile_contents += from_line
-                        replaced = True
-                    else:
-                        new_containerfile_contents += line
+            if not replaced:
+                new_containerfile_contents = from_line + new_containerfile_contents
+        else:
+            new_containerfile_contents = from_line
 
-                # If there was no FROM line, add it at the top
-                if not replaced:
-                    new_containerfile_contents = from_line + new_containerfile_contents
-            else:
-                new_containerfile_contents = from_line
+        for action in actions:
+            new_containerfile_contents += f"{action}\n"
 
-            for action in actions:
-                new_containerfile_contents += f"{action}\n"
+        return new_containerfile_contents
 
-            with open(containerfile, 'w') as f:
-                f.write(new_containerfile_contents)
+    def write_containerfile(self, new_containerfile_contents):
+        with open('/var/Containerfile', 'w') as f:
+            f.write(new_containerfile_contents)
 
-            shutil.copy("/var/Containerfile", "/var/.Containerfile")
+        shutil.copy("/var/Containerfile", "/var/.Containerfile")
 
-            print("Building bootc container")
-            subprocess.run(['podman', 'build', '-t', 'os', '/var'], check=True)
-            image_id = subprocess.run(['podman', 'images', '-q', 'localhost/os:latest'], capture_output=True, text=True, check=True)
-            subprocess.run(['bootc', 'switch', '--transport', 'containers-storage', image_id.stdout.strip()], check=True)
+    def build_and_switch_container(self):
+        print("Building bootc container")
+        subprocess.run(['podman', 'build', '-t', 'os', '/var'], check=True)
+
+        image_id = subprocess.run(
+            ['podman', 'images', '-q', 'localhost/os:latest'],
+            capture_output=True, text=True, check=True
+        )
+
+        subprocess.run(
+            ['bootc', 'switch', '--transport', 'containers-storage', image_id.stdout.strip()],
+            check=True
+        )
 
